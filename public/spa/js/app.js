49104a11fb97d988b0bac452e7ec37eb0f8aebdc
Jihan Nabilah <jihannabilah624@gmail.com>
2026-06-22 12:34:14 +0700

    update project

--- public/spa/js/app.js
+++ public/spa/js/app.js
@@ -1,5 +1,5 @@
 (function () {
-  const assetVersion = "20260615-2";
+  const assetVersion = "20260622-1";
   const componentFallbacks = {
     "#toast-mount": "",
     "#footer-mount": `<footer class="bg-[#0F172A] text-white"><div class="mx-auto max-w-7xl px-4 py-10 text-sm text-slate-400 sm:px-6 lg:px-8"><strong class="text-lg text-white">BarangBareng</strong><p class="mt-2">Marketplace sewa dan pinjam barang mahasiswa.</p></div></footer>`,
